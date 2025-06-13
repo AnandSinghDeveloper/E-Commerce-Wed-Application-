692444bcd40a7478f3854516d2aa897c164a2cfc
Anand Thakur <anandsingh23112005@gmail.com>
2025-06-13 10:37:06 +0530

    payment gateway build

---
+++ Backend/models/Oder.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+const Address = require("./Address");
+
+const OderSchema = new mongoose.Schema({
+  userId: String,
+  cartitems: [
+    {
+      productId: String,
+      title: String,
+      image: String,
+      price: String,
+      sellingPrice: String,
+      quantity: Number,
+    },
+  ],
+  AddressInfo: {
+    addressId: String,
+    address: String,
+    city: String,
+    pincode: String,
+    phone: String,
+    notes: String,
+  },
+  orderStatus: String,
+  paymentStatus: String,
+  paymentMethod: String,
+  paymentId: String,
+  payerID: String,
+  orderDate: String,
+  oderUpdatedDate: String,
+  totalAmount: String,
+});
+
+module.exports = mongoose.model("Oder", OderSchema);
